f8778279d4008e899dc042ccb7e4cd2588e3fbb9
sameerror <dangol.sameer@gmail.com>
2017-06-26 18:06:04 +0545

    database seeder comented

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        //  $this->call(UsersTableSeeder::class);
-       // $this->call(UsersSeeder::class);
-        $this->call(ReportTableSeeder::class);
-        $this->call(SliderTableSeeder::class);
+        $this->call(UsersSeeder::class);
+       // $this->call(ReportTableSeeder::class);
+      //  $this->call(SliderTableSeeder::class);
     }
 }
