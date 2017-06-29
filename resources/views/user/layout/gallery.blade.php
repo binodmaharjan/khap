b729176d6ca4ff830ad8f033533d87ae26462882
Bikram <bikram.shrestha@e-et-t.com>
2017-06-29 11:58:39 +0545

    update the content and remove gallery from top page.

--- resources/views/user/layout/gallery.blade.php
+++ resources/views/user/layout/gallery.blade.php
@@ -3,68 +3,13 @@
         <div class="row">
              <div class="col-md-12">
             <div class="col-md-8 padding-clear">
-                <div class="padding-top">
-                    <h3 class="otherTitles noShadow">Gallery</h3>
-                    <div class="custom-gallery-wraper gallaryOverflow">
-
-
-                        <ul class="first">
-
-
-                            <li>
-                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
-                                <p class="text">a</p>
-                            </li>
-                            <li>
-                                <img alt="Title  here"  src=" {{url('src/kids.jpg')}}">
-                                <p class="text">b</p>
-                            </li>
-                            <li>
-                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
-                                <p class="text">What we have to show</p>
-                            </li>
-                            <li>
-                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
-                                <p class="text">What we have to show</p>
-                            </li>
-                            <li>
-                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
-                                <p class="text">What we have to show</p>
-                            </li>
-                            <li>
-                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
-                                <p class="text">What we have to show</p>
-                            </li>
-                               <li>
-                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
-                                <p class="text">What we have to show</p>
-                            </li>
-                            <li>
-                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
-                                <p class="text">What we have to show</p>
-                            </li>
-                            <li>
-                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
-                                <p class="text">What we have to show</p>
-                            </li>
-                            <li>
-                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
-                                <p class="text">What we have to show</p>
-                            </li>
-
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 widget-sidebar ">
-                        <ul class="nav-tabs noShadow otherTitles reports">
+                
+                                        <ul class="nav-tabs noShadow otherTitles reports">
                             <li class='active'><a data-toggle="tab" href="#notice">Annual Report</a></li>
                             <li><a data-toggle="tab" href="#vacancy">Publication</a></li>
                             <li><a data-toggle="tab" href="#info">Notice</a></li>
                         </ul>
-                 </div>
-            <div class="col-md-4">
-                    <div class="widget-inner widget_comments tab-content staffSlider">
+                         <div class="widget-inner widget_comments tab-content staffSlider">
                         <div id="notice" class="tab-pane fade in active widget-inner ">
                             <ul class="comment reportList" >
                                 <li>
@@ -166,7 +111,105 @@
                             </ul>
                         </div>
                     </div>
+             <!--   <div class="padding-top">
+                    <h3 class="otherTitles noShadow">Gallery</h3>
+                    <div class="custom-gallery-wraper gallaryOverflow">
+
+
+                        <ul class="first">
+
+
+                            <li>
+                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
+                                <p class="text">a</p>
+                            </li>
+                            <li>
+                                <img alt="Title  here"  src=" {{url('src/kids.jpg')}}">
+                                <p class="text">b</p>
+                            </li>
+                            <li>
+                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
+                                <p class="text">What we have to show</p>
+                            </li>
+                            <li>
+                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
+                                <p class="text">What we have to show</p>
+                            </li>
+                            <li>
+                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
+                                <p class="text">What we have to show</p>
+                            </li>
+                            <li>
+                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
+                                <p class="text">What we have to show</p>
+                            </li>
+                               <li>
+                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
+                                <p class="text">What we have to show</p>
+                            </li>
+                            <li>
+                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
+                                <p class="text">What we have to show</p>
+                            </li>
+                            <li>
+                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
+                                <p class="text">What we have to show</p>
+                            </li>
+                            <li>
+                                <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
+                                <p class="text">What we have to show</p>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div> -->
+            </div>
+            <div class="col-md-4 widget-sidebar padding-top ">
+                                <div class="widget widget_comments noShadow">
+                    <h3 class="widget-title">Main Links</h3>
+                    <div class="widget-inner staffSlider">
+                        <ul class="comment mainlinks">
+                             <li>
+                                    <a href="#">link 1</a>
+                                </li>
+                                  <li>
+                                    <a href="#">link 1</a>
+                                </li>
+                                  <li>
+                                    <a href="#">link 1</a>
+                                </li>
+                                  <li>
+                                    <a href="#">link 1</a>
+                                </li>
+                                  <li>
+                                    <a href="#">link 1</a>
+                                </li>
+                                  <li>
+                                    <a href="#">link 1</a>
+                                </li>
+                                  <li>
+                                    <a href="#">link 1</a>
+                                </li>
+                                  <li>
+                                    <a href="#">link 1</a>
+                                </li>
+                                  <li>
+                                    <a href="#">link 1</a>
+                                </li>
+                                
+
+                            @if(count($news))
+                                @foreach($news as $news)
+                                <li>
+                                    <a href="#">{{$news->title}}</a>
+                                </li>
+                                @endforeach
+                                @endif
+
+                        </ul>
+                    </div>
                 </div>
+                 </div>
             </div>
         </div>
          </div>
--- resources/views/user/layout/member.blade.php
+++ resources/views/user/layout/member.blade.php
@@ -12,45 +12,11 @@
           <div class="text-center-one staffDetails">
                     <h3>BIkram</h3>
                      <h4>Staff</h4>
-                    <p>यो कार्यक्रम एउटा नमुना परियोजनाको एउटा  कार्यक्रम एउटा नमुना परियोजनाको रुपमा सञ्चालन भईरहेको छ, जसको उद्देश्य मूर्त एवं अमूर्त सांस्कृतिक एवं पुरातात्विक सम्पदाहरुको संरक्षण सम्बर्द्धनको साथै सांस्कृतिक एवं हस्तकलाको ब्यवसायिक प्रबर्द्धन र सांस्कृतिक पर्यटनलाई विकास गरि सहरी गरिबीन्यनिकरण गर्ने रहेक</p>
-
+                    <p><strong>cbmaharjan@yahoo.com</strong><p>
+                     <p><strong>9851022723</strong><p>
                     <a id="moreStaffs" class="size-mantain"><i>अन्य​ कर्मचारीहरु</i></a>
                 </div>
                 </div>
-                   </div>
-
-                   <div id='otherStaffs'>
-                     <div class="col-sm-12">
-                        <div class="col-sm-6 col-md-offset-3">
-                            <div class="ratio img-responsive img-circle"><img src="{{url('src/profile.jpg')}}">
-
-                        </div>
-                    </div>
-                    <div class="col-sm-12">
-                      <div class="text-center-one staffDetails">
-                                <h3>BIkram</h3>
-                                 <h4>Staff</h4>
-                                <p>यो कार्यक्रम एउटा नमुना परियोजनाको रुपमा सञ्चालन भयो कार्यक्रम एउटा नमुना परियोजनाको रुपमा सञ्चालन भयो कार्यक्रम एउटा नमुना परियोजनाको रुपमा सञ्चालन भईरहेको छ, जसको उद्देश्य मूर्त एवं अमूर्त सांस्कृतिक एवं पुरातात्विक सम्पदाहरुको संरक्षण सम्बर्द्धनको साथै सांस्कृतिक एवं हस्तकलाको ब्यवसायिक प्रबर्द्धन र सांस्कृतिक पर्यटनलाई विकास गरि सहरी गरिबीन्यनिकरण गर्ने रहेक</p>
-                            </div>
-                            </div>
-                      </div>
-                       <div class="col-sm-12">
-                        <div class="col-sm-6 col-md-offset-3">
-                            <div class="ratio img-responsive img-circle"><img src="{{url('src/profile.jpg')}}">
-
-                        </div>
-                    </div>
-                    <div class="col-sm-12">
-                      <div class="text-center-one staffDetails">
-                                <h3>BIkram</h3>
-                                 <h4>Staff</h4>
-                                <p>यो कार्यक्रम एउटा नमुना परियोजनाको रुपमा सञ्चालन भयो कार्यक्रम एउटा नमुना परियोजनाको रुपमा सञ्चालन भयो कार्यक्रम एउटा नमुना परियोजनाको रुपमा सञ्चालन भईरहेको छ, जसको उद्देश्य मूर्त एवं अमूर्त सांस्कृतिक एवं पुरातात्विक सम्पदाहरुको संरक्षण सम्बर्द्धनको साथै सांस्कृतिक एवं हस्तकलाको ब्यवसायिक प्रबर्द्धन र सांस्कृतिक पर्यटनलाई विकास गरि सहरी गरिबीन्यनिकरण गर्ने रहेक</p>
-                            </div>
-                            </div>
-                      </div>
-                       <div class="col-sm-12 text-center-one">
-                       <a id="hideStaffs" class="size-mantain"><i>hide more staff</i></a>
-                   </div>
                    </div>
 
 
