e0283215cc11acffb58a6fd78cd82ad62b8407a7
Bikram <bikram.shrestha@e-et-t.com>
2017-06-30 14:11:19 +0545

    make news  page responsive.

--- resources/views/user/layout/category.blade.php
+++ resources/views/user/layout/category.blade.php
@@ -10,81 +10,97 @@
                     <h3 class="widget-title">सुचनाहरु</h3>
 
 
-                    <div class="col-md-12 widget-sidebar newsDetails">
+                    <div class="row widget-sidebar newsDetails">
                      <div class="col-md-4">
                     <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
                   </div>
                     <div class="col-md-8">
-                       <div class="col-md-12 padding-clear">
-                    <h3> काठमाडौं नेपालको</h3><div class="paragraph-link floatRight postDate">२०७०,जेष्ठ १७</div>
+                  <div class="row margin-clear">
+                    <h3> काठमाडौं नेपालको</h3>
+                    <div class="paragraph-link floatRight postDate">२०७०,जेष्ठ १७</div>
+                  </div>
+                    <p> काठमाडौं नेपालको केन्द्रीय राजधानी शहर तथा मुलुककै पहिलो महानगर पनि हो । यस महानगरको उपनगरको रुपमा रहेका दुई ठूला शहरहरु मध्ये दक्षिणतर्फ ललितपुर र पूर्वतर्फ भक्तपुर रहेका छन्
+                        शिवपुरी, फुल्चोकी, नागार्जुङ र चन्द्रागिरी नामका चार ठूला पहाडहरुको बीचमा
+                        काठमाडौं नेपालको केन्द्रीय राजधानी शहर तथा मुलुककै पहिलो महानगर पनि हो । यस महानगरको उपनगरको रुपमा रहेका दुई ठूला शहरहरु मध्ये दक्षिणतर्फ ललितपुर र पूर्वतर्फ भक्तपुर रहेका छन्
+                        शिवपुरी, फुल्चोकी, नागार्जुङ र चन्द्रागिरी नामका चार ठूला पहाडहरुको बीचमा
+                        काठमाडौं नेपालको केन्द्रीय राजधानी शहर तथा मुलुककै पहिलो महानगर पनि हो । यस महानगरको उपनगरको रुपमा रहेका दुई ठूला शहरहरु मध्ये दक्षिणतर्फ ललितपुर र पूर्वतर्फ भक्तपुर रहेका छन्
+                        शिवपुरी, फुल्चोकी, नागार्जुङ र चन्द्रागिरी नामका चार ठूला पहाडहरुको बीचमा
+                        काठमाडौं नेपालको केन्द्रीय राजधानी शहर तथा मुलुककै पहिलो महानगर पनि हो । यस महानगरको उपनगरको रुपमा रहेका दुई ठूला शहरहरु मध्ये दक्षिणतर्फ ललितपुर र पूर्वतर्फ भक्तपुर रहेका छन्
+                        शिवपुरी, फुल्चोकी, नागार्जुङ र चन्द्रागिरी नामका चार ठूला पहाडहरुको बीचमा
+                    </p>
+                    <p><a href="src/profile.pdf" target='_blank'><i class="fa fa-file-pdf-o pdfColor" aria-hidden="true"></i> pdf</a></p>
+                      <a href="#" class='loadMore'>More Details</a>
+                </div>
+                 </div>
+
+                <div class="row widget-sidebar newsDetails">
+                     <div class="col-md-4">
+                    <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
+                  </div>
+                    <div class="col-md-8">
+                  <div class="row margin-clear">
+                    <h3> काठमाडौं नेपालको</h3>
+                    <div class="paragraph-link floatRight postDate">२०७०,जेष्ठ १७</div>
                   </div>
                     <p> काठमाडौं नेपालको केन्द्रीय राजधानी शहर तथा मुलुककै पहिलो महानगर पनि हो । यस महानगरको उपनगरको रुपमा रहेका दुई ठूला शहरहरु मध्ये दक्षिणतर्फ ललितपुर र पूर्वतर्फ भक्तपुर रहेका छन्
                         शिवपुरी, फुल्चोकी, नागार्जुङ र चन्द्रागिरी नामका चार ठूला पहाडहरुको बीचमा
                     </p>
                     <p><a href="src/profile.pdf" target='_blank'><i class="fa fa-file-pdf-o pdfColor" aria-hidden="true"></i> pdf</a></p>
-                     <a href="#" class='loadMore'>More Details</a>
+                      <a href="#" class='loadMore'>More Details</a> 
                 </div>
                  </div>
-                  <div class="col-md-12 widget-sidebar newsDetails">
+
+                  <div class="row widget-sidebar newsDetails">
                      <div class="col-md-4">
                     <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
                   </div>
                     <div class="col-md-8">
-                       <div class="col-md-12 padding-clear">
-                    <h3> काठमाडौं नेपालको</h3><div class="paragraph-link floatRight postDate">२०७०,जेष्ठ १७</div>
+                  <div class="row margin-clear">
+                    <h3> काठमाडौं नेपालको</h3>
+                    <div class="paragraph-link floatRight postDate">२०७०,जेष्ठ १७</div>
                   </div>
                     <p> काठमाडौं नेपालको केन्द्रीय राजधानी शहर तथा मुलुककै पहिलो महानगर पनि हो । यस महानगरको उपनगरको रुपमा रहेका दुई ठूला शहरहरु मध्ये दक्षिणतर्फ ललितपुर र पूर्वतर्फ भक्तपुर रहेका छन्
                         शिवपुरी, फुल्चोकी, नागार्जुङ र चन्द्रागिरी नामका चार ठूला पहाडहरुको बीचमा
                     </p>
                     <p><a href="src/profile.pdf" target='_blank'><i class="fa fa-file-pdf-o pdfColor" aria-hidden="true"></i> pdf</a></p>
-                     <a href="#" class='loadMore'>More Details</a>
+                      <a href="#" class='loadMore'>More Details</a> 
                 </div>
                  </div>
-                  <div class="col-md-12 widget-sidebar newsDetails">
+
+                  <div class="row widget-sidebar newsDetails">
                      <div class="col-md-4">
                     <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
                   </div>
                     <div class="col-md-8">
-                       <div class="col-md-12 padding-clear">
-                    <h3> काठमाडौं नेपालको</h3><div class="paragraph-link floatRight postDate">२०७०,जेष्ठ १७</div>
+                  <div class="row margin-clear">
+                    <h3> काठमाडौं नेपालको</h3>
+                    <div class="paragraph-link floatRight postDate">२०७०,जेष्ठ १७</div>
                   </div>
                     <p> काठमाडौं नेपालको केन्द्रीय राजधानी शहर तथा मुलुककै पहिलो महानगर पनि हो । यस महानगरको उपनगरको रुपमा रहेका दुई ठूला शहरहरु मध्ये दक्षिणतर्फ ललितपुर र पूर्वतर्फ भक्तपुर रहेका छन्
                         शिवपुरी, फुल्चोकी, नागार्जुङ र चन्द्रागिरी नामका चार ठूला पहाडहरुको बीचमा
                     </p>
                     <p><a href="src/profile.pdf" target='_blank'><i class="fa fa-file-pdf-o pdfColor" aria-hidden="true"></i> pdf</a></p>
-                     <a href="#" class='loadMore'>More Details</a>
+                      <a href="#" class='loadMore'>More Details</a> 
                 </div>
                  </div>
-                  <div class="col-md-12 widget-sidebar newsDetails">
+
+                  <div class="row widget-sidebar newsDetails">
                      <div class="col-md-4">
                     <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
                   </div>
                     <div class="col-md-8">
-                       <div class="col-md-12 padding-clear">
-                    <h3> काठमाडौं नेपालको</h3><div class="paragraph-link floatRight postDate">२०७०,जेष्ठ १७</div>
+                  <div class="row margin-clear">
+                    <h3> काठमाडौं नेपालको</h3>
+                    <div class="paragraph-link floatRight postDate">२०७०,जेष्ठ १७</div>
                   </div>
                     <p> काठमाडौं नेपालको केन्द्रीय राजधानी शहर तथा मुलुककै पहिलो महानगर पनि हो । यस महानगरको उपनगरको रुपमा रहेका दुई ठूला शहरहरु मध्ये दक्षिणतर्फ ललितपुर र पूर्वतर्फ भक्तपुर रहेका छन्
                         शिवपुरी, फुल्चोकी, नागार्जुङ र चन्द्रागिरी नामका चार ठूला पहाडहरुको बीचमा
                     </p>
                     <p><a href="src/profile.pdf" target='_blank'><i class="fa fa-file-pdf-o pdfColor" aria-hidden="true"></i> pdf</a></p>
-                     <a href="#" class='loadMore'>More Details</a>
+                      <a href="#" class='loadMore'>More Details</a> 
                 </div>
                  </div>
-                  <div class="col-md-12 widget-sidebar newsDetails">
-                     <div class="col-md-4">
-                    <img alt="Title goes here"  src=" {{url('src/kids.jpg')}}">
-                  </div>
-                    <div class="col-md-8">
-                       <div class="col-md-12 padding-clear">
-                    <h3> काठमाडौं नेपालको</h3><div class="paragraph-link floatRight postDate">२०७०,जेष्ठ १७</div>
-                  </div>
-                    <p> काठमाडौं नेपालको केन्द्रीय राजधानी शहर तथा मुलुककै पहिलो महानगर पनि हो । यस महानगरको उपनगरको रुपमा रहेका दुई ठूला शहरहरु मध्ये दक्षिणतर्फ ललितपुर र पूर्वतर्फ भक्तपुर रहेका छन्
-                        शिवपुरी, फुल्चोकी, नागार्जुङ र चन्द्रागिरी नामका चार ठूला पहाडहरुको बीचमा
-                    </p>
-                    <p><a href="src/profile.pdf" target='_blank'><i class="fa fa-file-pdf-o pdfColor" aria-hidden="true"></i> pdf</a></p>
-                     <a href="#" class='loadMore'>More Details</a>
-                </div>
-                 </div>
+
 
 
                 </div>
