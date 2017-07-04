7cfbb2bbc261585be60a49e40fdbd9d00f35776f
Binod Maharjan <maharjan411@gmail.com>
2017-07-04 18:26:46 +0545

    scroll css removed from dashboard

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,50 +1,55 @@
 <nav class="navbar-default navbar-side" role="navigation" style="left: 0px;">
     <div class="sidebar-collapse">
-        <ul class="nav" id="main-menu">
 
-            <li>
-                <a  href="/home"><i class="fa fa-dashboard"></i> Dashboard</a>
-            </li>
-            <li>
-                <a href="{{route('admin_category')}}"><i class="fa fa-th-list"></i> Categories</a>
-            </li>
+        @php
+            $activeRoute = Route::currentRouteName()
+        @endphp
+            <ul class="nav" id="main-menu">
 
-            <li>
-                <a href="{{route('admin_menu')}}"><i class="fa fa-bars"></i> Nav Menu</a>
-            </li>
-            <li>
-                <a href="{{route('admin_articles')}}"><i class="fa fa-newspaper-o"></i>Articles</a>
-            </li>
-            <li>
-                <a href="{{route('admin_sliders')}}"><i class="fa fa-sliders"></i> Slider</a>
-            </li>
+                <li class="{{strcmp($activeRoute,'home')==0?'active-menu':''}}">
+                    <a  href="/home"><i class="fa fa-dashboard" ></i> Dashboard</a>
+                </li>
+                <li class="{{strcmp($activeRoute,'admin_category')==0?'active-menu':''}}">
+                    <a href="{{route('admin_category')}}"><i class="fa fa-th-list"></i> Categories</a>
+                </li>
 
-
-            <li>
-                <a href="{{route('admin_gallery')}}"><i class="fa fa-picture-o"></i> Gallery</a>
-            </li>
-
-            <li>
-                <a href="{{route('admin_reports')}}"><i class="fa fa-edit"></i> Report </a>
-            </li>
-
-            <li>
-                <a href="{{route('admin_members')}}"><i class="fa fa-user "></i> Members</a>
-            </li>
-
-            <li>
-                <a href="{{route('admin_supports')}}"><i class="fa fa-edit"></i> फारम </a>
-            </li>
+                <li class="{{strcmp($activeRoute,'admin_menu')==0?'active-menu':''}}">
+                    <a href="{{route('admin_menu')}}"><i class="fa fa-bars"></i> Nav Menu</a>
+                </li>
+                <li class="{{strcmp($activeRoute,'admin_articles')==0?'active-menu':''}}">
+                    <a href="{{route('admin_articles')}}"><i class="fa fa-newspaper-o"></i>Articles</a>
+                </li>
+                <li class="{{strcmp($activeRoute,'admin_sliders')==0?'active-menu':''}}">
+                    <a href="{{route('admin_sliders')}}"><i class="fa fa-sliders"></i> Slider</a>
+                </li>
 
 
-            <li>
-                <a href="{{route('admin_gunaso')}}"><i class="fa fa-comment"></i> गुनासो  </a>
-            </li>
+                <li class="{{strcmp($activeRoute,'admin_gallery')==0?'active-menu':''}}">
+                    <a href="{{route('admin_gallery')}}"><i class="fa fa-picture-o"></i> Gallery</a>
+                </li>
 
-            <li>
-                <a href="https://webthemez.com/demo/marvel-bootstrap-html-admin-template/empty.html"><i class="fa fa-fw fa-file"></i> Empty Page</a>
-            </li>
-        </ul>
+                <li class="{{strcmp($activeRoute,'admin_reports')==0?'active-menu':''}}">
+                    <a href="{{route('admin_reports')}}"><i class="fa fa-edit"></i> Report </a>
+                </li>
+
+                <li class="{{strcmp($activeRoute,'admin_members')==0?'active-menu':''}}">
+                    <a href="{{route('admin_members')}}"><i class="fa fa-user "></i> Members</a>
+                </li>
+
+                <li class="{{strcmp($activeRoute,'admin_supports')==0?'active-menu':''}}">
+                    <a href="{{route('admin_supports')}}"><i class="fa fa-edit"></i> फारम </a>
+                </li>
+
+
+                <li class="{{strcmp($activeRoute,'admin_gunaso')==0?'active-menu':''}}">
+                    <a href="{{route('admin_gunaso')}}"><i class="fa fa-comment"></i> गुनासो  </a>
+                </li>
+
+                <li >
+                    <a href="https://webthemez.com/demo/marvel-bootstrap-html-admin-template/empty.html"><i class="fa fa-fw fa-file"></i> Empty Page</a>
+                </li>
+            </ul>
+
 
     </div>
 
