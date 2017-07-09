8b2c16368a3b8ff0e725939d50a99ca826aa8584
Binod Maharjan <maharjan411@gmail.com>
2017-07-09 10:17:28 +0545

    db seed

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -14,9 +14,9 @@ class UsersSeeder extends Seeder
         //
 
         DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('admin'),
+            'name' => 'Shree Gopal',
+            'email' => 'lmcity.ward3@gmail.com',
+            'password' => bcrypt('shree123'),
         ]);
     }
 }
