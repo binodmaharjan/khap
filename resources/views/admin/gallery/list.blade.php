be39b0a29c7cceed7b8fd13b4941b3412df65c4e
Binod Maharjan <maharjan411@gmail.com>
2017-09-18 19:09:31 +0545

    copy link added

--- resources/views/admin/gallery/list.blade.php
+++ resources/views/admin/gallery/list.blade.php
@@ -37,8 +37,13 @@
                     <td><img src="{{url('storage/'.$slider->path)}}" height="50px"></td>
                     <td><a href="{{route('admin_gallery_edit',array($slider->id))}}"> {{$slider->title}}</a></td>
                     <td>
-                        <a href="{{url('downloads/'.$slider->path)}}"> <button ><i class="fa fa-download" aria-hidden="true"> </i> Download</button></a>
+                         <a href="{{url('downloads/'.$slider->path)}}"> <button ><i class="fa fa-download" aria-hidden="true"> </i> Download</button></a>
                         <a href="{{route('admin_gallery_delete',array($slider->id))}}" onclick="return confirm('Are you sure you want to delete this item?');">  <button ><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button></a>
+
+                    <a><button class="clipboard" data-clipboard-text="{{url('storage/'.$slider->path)}}"><i
+                                    class="fa fa-clipboard" aria-hidden="true">Copy link </i></button>
+                    </a>
+
                     </td>
 
                 </tr>
